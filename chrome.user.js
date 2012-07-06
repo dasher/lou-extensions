ff2be68c112f78980b2c6399e4ddb786f5bde51c
Bart Jolling <bart_jolling@hotmail.com>
2012-07-06 17:54:14 +0200

    Loader used manifest as input

--- chrome.user.js
+++ chrome.user.js
@@ -1,28 +1,87 @@
-function loadjscssfile(filename, filetype){
-	if (filetype=="js"){ //if filename is a external JavaScript file
-	  var fileref=document.createElement('script')
-	  fileref.setAttribute("type","text/javascript")
-	  fileref.setAttribute("src", filename)
-	}
-	else if (filetype=="css"){ //if filename is an external CSS file
-	  var fileref=document.createElement("link")
-	  fileref.setAttribute("rel", "stylesheet")
-	  fileref.setAttribute("type", "text/css")
-	  fileref.setAttribute("href", filename)
-	}
-	if (typeof fileref!="undefined")
-	  document.getElementsByTagName("head")[0].appendChild(fileref)
+/// Dynamic script loader for lou-extensions in Google Chrome.
+/**
+ * @namespace lou_extensions
+ */
+(function (window, undefined) {
+    var lou_extensions = window.lou_extensions = {};
+
+    /**
+     * Injects a link to a javascript file in the HEAD of a document.
+     * @param {string} url - location of the javascript file to inject.
+     * */
+    lou_extensions.injectJavascriptFile = function (url) {
+        var scriptElement = document.createElement('script');
+        scriptElement.setAttribute("type", "text/javascript");
+        scriptElement.setAttribute("src", url);
+
+        if (typeof scriptElement != "undefined")
+            document.getElementsByTagName("head")[0].appendChild(scriptElement);
+    };
+
+    /**
+     * Injects a link to a stylesheet in the HEAD of a document.
+     * @param {string} url - location of the javascript file to inject.
+     * */
+    lou_extensions.injectStylesheet = function (url) {
+        var linkElement = document.createElement("link");
+        linkElement.setAttribute("rel", "stylesheet");
+        linkElement.setAttribute("type", "text/css");
+        linkElement.setAttribute("href", url);
+
+        if (typeof linkElement != "undefined")
+            document.getElementsByTagName("head")[0].appendChild(linkElement);
+    };
+
+    /**
+     * Analyse the manifest.json to find the accessible js and css and injects them.
+     * @param manifest
+     */
+    lou_extensions.loadExtensions = function (manifest) {
+        if (manifest != null && manifest.web_accessible_resources != null) {
+            for (var i = 0; i < manifest.web_accessible_resources.length; i++) {
+                try {
+                    var resource = manifest.web_accessible_resources[i];
+
+                    if (resource.indexOf('.js', resource.length - 3) !== -1) {
+                        GM_log('[lou-extensions] - ' + resource);
+                        lou_extensions.injectJavascriptFile(chrome.extension.getURL(resource));
+
+                    } else if (resource.indexOf('.css', resource.length - 4) !== -1) {
+                        GM_log('[lou-extensions] - ' + resource);
+                        lou_extensions.injectStylesheet(chrome.extension.getURL(resource));
+                    }
+                } catch (e) {
+                    GM_log('[lou-extensions] - ' + e)
+                }
+            }
+        }
+    };
+
+    window["lou_extensions"] = lou_extensions;
+})(window);
+
+/**
+ * Main function
+ */
+try {
+    GM_log('[lou-extensions] Loading LOU extensions');
+
+    if (chrome != undefined && chrome.extension != undefined) {
+
+        // load extensions manifest.json file
+        var url = chrome.extension.getURL('manifest.json');
+
+        GM_xmlhttpRequest({
+            "url":url,
+            "onload":function (xhr) {
+                var manifest = JSON.parse(xhr.responseText);
+                lou_extensions.loadExtensions(manifest);
+            }
+        });
+    } else {
+        GM_log('[lou-extensions] Chrome extensions not available. No lou-extensions were injected');
+    }
+} catch (e) {
+    GM_log('[lou-extensions]' + e);
 }
 
-try {
-	GM_log('LOU Bos Community');
-	var url = chrome.extension.getURL('twitter.js');
-	GM_log(url);
-	loadjscssfile(url, "js");
-	GM_log('loaded');
-} catch (e) {
-	GM_log('+catch+');
-	if (typeof console != 'undefined') console.log(e);
-	else if (window.opera) opera.postError(e);
-	else GM_log(e);
-}
