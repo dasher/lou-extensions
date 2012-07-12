f954d2d31f48fe4dc8f98f1cc891d9077b802b20
Bart Jolling <bart_jolling@hotmail.com>
2012-07-12 13:42:08 +0200

    Release 0.2.0 Beta version for Google Chrome and Greasemonkey

--- greasemonkey.user.js
+++ greasemonkey.user.js
@@ -4,7 +4,7 @@
 // @description Collection of extensions for Lord of Ultima (http://www.lordofultima.com/)
 // @include     http://localhost*
 // @include     http://prodgame*.lordofultima.com/*
-// @version     0.1.0
+// @version     0.2.0
 // @run-at      document_end
 // @resource    bos/greasemonkey.light.js                    bos/greasemonkey.light.js
 // @resource    bos/sprintf-0.7-beta1.js                     bos/sprintf-0.7-beta1.js
@@ -90,8 +90,6 @@ function injectStyle (cssContent, mimeType, styleId) {
  * @param manifest
  */
 function loadExtensions(info) {
-
-    console.log(info.toSource());
 
     if (("resources" in info) &&  (info.resources !== null)) {
 
